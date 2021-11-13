82a7dc48566359d80e9266f04a347039802a4630
Lorea13 <lorea.intxausti@opendeusto.es>
2021-11-13 21:48:41 +0100

    21-48

--- appBookStore/admin.py
+++ appBookStore/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Autor, Genero, Editorial, Idioma, Libro
+admin.site.register(Autor)
+admin.site.register(Genero)
+admin.site.register(Editorial)
+admin.site.register(Idioma)
+admin.site.register(Libro)
+
