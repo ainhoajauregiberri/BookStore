e5821830222c2be695d10ae30e5d6b4333a0390c
noraroyo <noraroyo@opendeusto.es>
2021-11-13 23:14:52 +0100

    Models 23-14

--- appBookStore/models.py
+++ appBookStore/models.py
@@ -1,30 +1,47 @@
-from django.db import models
+from django.db import migrations, models
+
+
+import django.utils.datetime_safe
 from django.db.models.fields import FloatField
+
  
 class Autor(models.Model):
  # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
- nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return f"id={self.id}, nombre{self.nombre}"
 
 class Editorial(models.Model):
- nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
 # Campo para la relación one-to-many
+    def __str__(self):
+        return f"id={self.id}, nombre{self.nombre}"
+	
 
 class Genero(models.Model):
- nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
 # Campo para la relación one-to-many
+    def __str__(self):
+        return f"id={self.id}, nombre{self.nombre}"
+
 
 class Idioma(models.Model):
- nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
 # Campo para la relación one-to-many
+    def __str__(self):
+        return f"id={self.id}, nombre{self.nombre}"
 
 class Libro(models.Model):
  # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
- nombre = models.CharField(max_length=50)
- autores = models.ManyToManyField(Autor)
- editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
- genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
- idioma = models.ForeignKey(Idioma, on_delete=models.CASCADE)
- paginas = models.IntegerField(default=0)
+    nombre = models.CharField(max_length=50)
+    autores = models.ManyToManyField(Autor)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    idioma = models.ForeignKey(Idioma, on_delete=models.CASCADE)
+    paginas = models.IntegerField(default=0)
+    def __str__(self):
+        return f"id={self.id}, nombre={self.nombre}, autores={self.autores.all()}, editorial={self.editorial}, genero={self.genero}, idioma={self.idioma}, paginas={self.paginas}"
+
 
 
 
--- appBookStore/views.py
+++ appBookStore/views.py
@@ -1,8 +1,40 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Autor, Genero, Idioma, Editorial, Libro
 
 
-
-#devuelve el listado de empresas
+#devuelve el listado de libros de cada editorial
 def index(request):
-	return HttpResponse("kaixo")
+	departamentos = Departamento.objects.order_by('nombre')
+	output = ', '.join([d.nombre for d in departamentos])
+	return HttpResponse(output)
+
+#devuelve los detalles de un LIBRO
+def detail(request, libros_id):
+	libro = Libro.objects.get(pk=libro_id)
+	output = ', '.join([str(libro.id), libro.nombre, libro.editorial, str([a.nombre for a in libro.autores.all()])])
+	return HttpResponse(output)
+
+#devuelve los detalles de EDITORIALES
+def editoriales(request, empleado_id):
+	editorial = Editorial.objects.get(pk=editorial_id)
+	output = ', '.join([str(editorial.id), editorial.nombre, str([l.nombre for l in editorial.libro_set.all()])])
+	return HttpResponse(output)
+
+#devuelve los detalles de los AUTORES
+def autores(request, departamento_id):
+	autor = Autor.objects.get(pk=autor_id)
+	output = ', '.join([str(autor.id), autor.nombre, str([l.nombre for l in autor.libro_set.all()])])
+	return HttpResponse(output)
+
+#devuelve los detalles de un empleado
+def empleado(request, empleado_id):
+	empleado = Empleado.objects.get(pk=empleado_id)
+	output = ', '.join([str(empleado.id), empleado.nombre, str(empleado.fecha_nacimiento), str(empleado.antiguedad), str(empleado.departamento), str([h.nombre for h in empleado.habilidades.all()])])
+	return HttpResponse(output)
+
+#devuelve los detalles de una habilidad
+def habilidad(request, habilidad_id):
+	habilidad = Habilidad.objects.get(pk=habilidad_id)
+	output = ', '.join([str(habilidad.id), habilidad.nombre, str([e.nombre for e in habilidad.empleado_set.all()])])
+	return HttpResponse(output)
