694767678f0d3231577f3ccb723f0aeb45fdf425
ainhoajauregiberri <ainhoaillarramendi@opendeusto.es>
2021-11-13 19:05:03 +0100

    19-04

--- appBookStore/models.py
+++ appBookStore/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.db.models.fields import FloatField
+ 
+class Autor(models.Model):
+ # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+ nombre = models.CharField(max_length=50)
 
-# Create your models here.
+class Editorial(models.Model):
+ nombre = models.CharField(max_length=50)
+# Campo para la relación one-to-many
 
+class Genero(models.Model):
+ nombre = models.CharField(max_length=50)
+# Campo para la relación one-to-many
+
+class Idioma(models.Model):
+ nombre = models.CharField(max_length=50)
+# Campo para la relación one-to-many
+
+class Libro(models.Model):
+ # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
+ nombre = models.CharField(max_length=50)
+ autores = models.ManyToManyField(Autor)
+ editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+ genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+ idioma = models.ForeignKey(Idioma, on_delete=models.CASCADE)
+ paginas = models.IntegerField(default=0)
+
+
+
+ 
+
+
+ 
