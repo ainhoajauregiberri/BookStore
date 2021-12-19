181e43a9619c3a87f25e90f6a9fcc0421da5b629
Lorea13 <lorea.intxausti@opendeusto.es>
2021-12-19 20:43:11 +0100

    20-43

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
  path('appBookStore/', include('appBookStore.urls')),
  path('admin/', admin.site.urls),
+ url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
 
