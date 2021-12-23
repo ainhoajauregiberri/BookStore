70d585a92f233a483ac3da755b15f6ed8ed99f7e
ainhoajauregiberri <ainhoaillarramendi@opendeusto.es>
2021-12-23 13:39:03 +0100

    js 13-39

---
+++ appBookStore/static/js/calificar.js
@@ -0,0 +1,27 @@
+function calificar(item){
+    var contador;
+    console.log(item);
+    contador=item.id[0]; //captura primer caracter del id
+    let nombre= item.id.substring(1);//captura toda la cadena despues del primer caracter (estrella)
+    for(let i=0; i <5; i ++){
+        if(i<contador){
+            document.getElementById((i+1)+nombre).style.color="orange";
+
+        }else{
+            document.getElementById((i+1)+nombre).style.color="black";
+        }
+    }
+};
+
+function mensaje(){
+    window.alert("¡Gracias por calificar este libro con " +contador+ " estrellas!");
+};
+
+function calcularMedia(){
+    var numValoraciones;
+    var media=1;
+    media=((libro.numValoraciones*media)+ contador)/(libro.numValoraciones+1);
+    return media;
+    libro.numValoraciones+=1;
+    return libro.numValoraciones;
+};
